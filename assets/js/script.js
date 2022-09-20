0e3c44bbcf15acee3f14381150acf40c70636944
MarMarMo <108547874+MMockus15@users.noreply.github.com>
2022-09-20 17:07:33 -0600

    api set up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,16 @@
 // create a request variable and assign a new object to it
 var cityInputEl = document.querySelector(".city-input");
 var formEl = document.querySelector(".form");
+// d91f911bcf2c0f925fb6535547a5ddc9
 
-var apiKey = "d91f911bcf2c0f925fb6535547a5ddc9";
+var apiKey = "840e9ff38c70dacbffbb1d3e4e259ed7";
+
 
 function fetchGeo(event) {
 	event.preventDefault()
   var cityName = cityInputEl.value;
   fetch(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`
+	"http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid-" + apiKey
   )
     .then((response) => {
       return response.json();
@@ -18,10 +20,13 @@ function fetchGeo(event) {
 	fetchWeather(data[0].lat, data[0].lon)
     });
 }
-function fetchWeather(lat, lon) {
+
+
+
+function fetchWeather(lat, lon, cityName) {
 console.log(lat, lon)
 fetch(
-    `http://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&cnt=6&appid=${apiKey}`
+    "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&cnt=6&appid=" + apiKey
   )
     .then((response) => {
       return response.json();
@@ -29,6 +34,11 @@ fetch(
     .then((data) => {
       console.log(data);
     });
+
+	var dayArray = [data.list[6], data.list[14], data.list[22], data.list[30], data.list[38]];
+
+	for (var i = 0; i < dayArray.length; i++) {
+		console.log(dayArray[i]);
+	}
 }
 formEl.addEventListener("submit", fetchGeo);
-
