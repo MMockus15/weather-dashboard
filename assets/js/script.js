8ef8e010eb39909018022d1b880f10b2b31ce61a
MarMarMo <108547874+MMockus15@users.noreply.github.com>
2022-09-21 10:51:09 -0600

    pulling data from api funtion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,14 @@
 // create a request variable and assign a new object to it
 var cityInputEl = document.querySelector(".city-input");
 var formEl = document.querySelector(".form");
-// d91f911bcf2c0f925fb6535547a5ddc9
-
-var apiKey = "840e9ff38c70dacbffbb1d3e4e259ed7";
+var apiKey = "840e9ff38c70dacbffbb1d3e4e259ed7"; 
 
 
 function fetchGeo(event) {
 	event.preventDefault()
   var cityName = cityInputEl.value;
   fetch(
-	"http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid-" + apiKey
+	"http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&appid=" + apiKey
   )
     .then((response) => {
       return response.json();
@@ -33,12 +31,21 @@ fetch(
     })
     .then((data) => {
       console.log(data);
+	  var dayArray = [data.list[6], data.list[14], data.list[22], data.list[30], data.list[38]];
+  
+	  for (var i = 0; i < dayArray.length; i++) {
+		  console.log(dayArray[i]);
+	  }
     });
 
-	var dayArray = [data.list[6], data.list[14], data.list[22], data.list[30], data.list[38]];
-
-	for (var i = 0; i < dayArray.length; i++) {
-		console.log(dayArray[i]);
-	}
 }
 formEl.addEventListener("submit", fetchGeo);
+
+
+displayWeather: function(data) {
+var { name } = data;
+var { icon, description } = data.weather[0];
+var { temp, humidity } = data.main;
+var { speed } = data.wind;
+console.log(name, icon, description, temp, humidity, speed);
+}
